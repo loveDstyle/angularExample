bdd0d4ce03e5c760bb5f5c93fff27c2a23f1fcbb
xyx <xyx>
2017-05-02 17:30:09 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './herodetail.component';
+
+@NgModule({
+  imports:      [
+            BrowserModule,
+            FormsModule
+            ],
+  declarations: [ AppComponent,
+            HeroDetailComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
